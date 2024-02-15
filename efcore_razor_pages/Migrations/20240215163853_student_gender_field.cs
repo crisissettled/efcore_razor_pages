79229b86411eb099ff4a998d730db540a0667945
James Liu <jliu@sirokygroup.com>
2024-02-15 11:54:48 -0500

    add student.gender field

---
+++ efcore_razor_pages/Migrations/20240215163853_student_gender_field.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace efcore_razor_pages.Migrations
+{
+    /// <inheritdoc />
+    public partial class student_gender_field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Students");
+        }
+    }
+}
--- efcore_razor_pages/MyModel/Models.cs
+++ efcore_razor_pages/MyModel/Models.cs
@@ -3,6 +3,8 @@
     public class Student
     {
         public int StudentId { get; set; }
+
+        public string Gender { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
